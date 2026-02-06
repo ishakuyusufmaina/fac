9547b68c23cda1a4da8f833f2363b4921664ac24
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-02-06 10:58:16 +0100

    Create batch2.js

---
+++ batch2.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+  apiKey: "AIzaSyCm_DSZaYVQCt0ubBwRkE9nOTryJayxH0w",
+  authDomain: "school-in-the-box.firebaseapp.com",
+  projectId: "school-in-the-box",
+  storageBucket: "school-in-the-box.firebasestorage.app",
+  messagingSenderId: "596350496353",
+  appId: "1:596350496353:web:84c66c351700ddf0824420",
+  measurementId: "G-3MGCDH4FRF"
+};
+
+const paybookConfig = {
+    apiKey: "AIzaSyAwrWSv_pD2keej4qIjx1jeeRq5K_DBKYA",
+    authDomain: "mainafly-school-fees.firebaseapp.com",
+    projectId: "mainafly-school-fees",
+    storageBucket: "mainafly-school-fees.firebasestorage.app",
+    messagingSenderId: "813090872440",
+    appId: "1:813090872440:web:b1b1bc39ab82256fa2a18d",
+    measurementId: "G-E6B3WQ6P8Q"
+  };
